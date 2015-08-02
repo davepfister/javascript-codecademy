47071f9d77403ec65f536bf52a7abeed7883540c
Dave Pfister <davepfister@comcast.net>
2015-08-02 17:10:31 -0400

    Origin
    original code

---
+++ oldenoughtoplay.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<script>
+		// Check if the user is ready to play!
+		confirm("I am ready to play!");
+
+		var age=prompt("What's your age?");
+
+		if(age < 13)
+		{
+			console.log("allowed to play but you take no responsibility.");
+		} 
+		else // "otherwise"
+		{
+			console.log("play on!")
+		}
+		</script>
+	</body>
+</html>
