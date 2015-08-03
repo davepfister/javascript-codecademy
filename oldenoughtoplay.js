a17d66da25251dd6e3c705560b074dd206b9f541
Dave Pfister <davepfister@comcast.net>
2015-08-02 20:15:04 -0400

    Add race if statement
    added an if statement for if you want to race.

--- oldenoughtoplay.js
+++ oldenoughtoplay.js
@@ -19,6 +19,17 @@
 		{
 			console.log("play on!")
 		}
+		console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+		console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+		if(userAnswer === 'yes') 
+		{
+			console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+		}
+		else
+		{
+			console.log("Oh no! Bieber shakes his head and sings 'I    set a pace, so I can race without pacing.'");
+		}   
 		</script>
 	</body>
 </html>
